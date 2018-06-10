621b4ca29fa580114a9864f3daa74e41fa4adb57
xue99999 <1090374167@qq.com>
2018-06-10 23:15:00 +0800

    scrapy init and setting.py change robot

---
+++ shiyanlougithub/pipelines.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from shiyanlougithub.models import Repository, engine
+
+
+class ShiyanlougithubPipeline(object):
+    def process_item(self, item, spider):
+        item['update_time'] = datetime.strptime(item['update_time'], '%Y-%m-%dT%H:%M:%SZ')
+        self.session.add(Repository(**item))
+        return item
+
+    def open_spider(self, spider):
+        Session = self.sessionmaker()
+        self.session = Session()
+
+    def close_spider(self, spider):
+        self.session.commit()
+        self.session.close()
---
+++ shiyanlougithub/spiders/repositories.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from shiyanlougithub.items import ShiyanlougithubItem
+
+
+class RepositoriesSpider(scrapy.Spider):
+    name = 'repositories'
+
+    @property
+    def start_urls(self):
+        url_tmp = 'https://github.com/shiyanlou?page={}&tab=respositories'
+        urls = (url_tmp.format(i) for i in range(1, 5))
+        return urls
+
+    def parse(self, response):
+        for rep in response.css('li.public'):
+            yield ShiyanlougithubItem({
+                'name': rep.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first('\n\s*(.*)'),
+                'update_time': rep.xpath('.//relative-time/@datetime').extract_first()
+                })
